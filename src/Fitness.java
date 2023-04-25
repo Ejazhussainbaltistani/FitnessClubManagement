8b23886937950d1b84bd04311dfb68f595c9e379
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2023-04-25 11:08:53 +0500

    Added functions Checkavailabeseats ,getFitnessClassByLesson,and showBookings for displaying all bookings

--- src/Fitness.java
+++ src/Fitness.java
@@ -1,4 +1,6 @@
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +29,48 @@ public class Fitness {
         
         return null;
     }
+    public static void showBookings()
+    {
+    	for(Booking b:bookings)
+    	{
+    		b.display();
+    	}
+    }
     
+    public FitnessClass getFitnessClassByLesson(String name) {
+       FitnessClass fitnessClass=null;
+        	for(FitnessClass f:fitnessClasses)
+        	{
+        		if(f.getLesson().equals(name)) {
+        			fitnessClass=f;
+        		}
+        	}
+       
+        
+        return fitnessClass; // indicate that the class is not available on the specified date and shift
+    }
+
+    
+    public int checkAvailableSeats(LocalDate date, String shift) {
+        int count = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for (Booking booking : bookings) {
+        	String id=booking.getClassId();
+        	String shifts="";
+        	for(FitnessClass f:fitnessClasses)
+        	{
+        		if(f.getClassId().equals(id)) {
+        			shifts=f.getShift();
+        			
+        		}
+        	}
+            if (booking.getDate().equals(date) && shifts.equals(shift)) {
+                count++;
+            }
+        }
+        
+        return 5-count; // indicate that the class is not available on the specified date and shift
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
